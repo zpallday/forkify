44d42cb215fa7bc819f48f36cabe3625f68223eb
Zach <zpman95@hotmail.com>
2020-07-13 17:27:43 -0500

    SearchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,27 @@
 import Search from './models/Search'
-
+import * as searchView from './views/seachView'
+import { elements, renderLoader, clearLoader } from './views/base'
 const state = {}
 
 const controlSearch = async () => {
-    const query = 'pizza'
-
+    const query = searchView.getInput();
+   console.log(query)
     if (query) {
         state.search = new Search(query);
+
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes)
+
         await state.search.getResults();
-        console.log(state.search.result)
+
+        clearLoader();
+        searchView.renderResults(state.search.result)
     }
+  
 }
 
-document.querySelector('.search').addEventListener("submit", e => {
+    elements.searchForm.addEventListener("submit", e => {
     e.preventDefault();
     controlSearch();
 })
