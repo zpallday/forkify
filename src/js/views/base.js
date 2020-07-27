f4943bd0d6a08315d15a7a3380adc28e9f10f0ad
Zach <zpman95@hotmail.com>
2020-07-27 13:55:00 -0500

    Finished

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,21 +1,26 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list'),
-    searchRes: document.querySelector('.results')
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list'),
+    likesMenu: document.querySelector('.likes__field'),
+    likesList: document.querySelector('.likes__list')
 };
 
 export const elementStrings = {
     loader: 'loader'
-}
+};
 
 export const renderLoader = parent => {
     const loader = `
-    <div class="${elementStrings.loader}">
-     <svg>
-        <use href="img/icons.svg#icon-cw"></use>
-        </svg>
-    </div>
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
     `;
     parent.insertAdjacentHTML('afterbegin', loader);
 };
